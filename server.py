b5df3b75c1661c4a49f2e3f2974be3eca93dd8f0
admin <roman.shein.07@mail.ru>
2025-05-07 21:44:42 +0300

    add buttons for job with bot

--- server.py
+++ server.py
@@ -19,10 +19,11 @@ import logging
 import urllib
 from datetime import datetime
 from aiogram import Bot, Dispatcher, types
-from aiogram.dispatcher.filters import Command
-from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.filters import Command
+from aiogram.filters.state import State, StatesGroup
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardRemove
 from data.user import UserCard
 from config import BOT_TOKEN
 from data import db_session
@@ -34,8 +35,43 @@ logging.basicConfig(
 )
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
-API_URL = 'https://imminent-jet-suggestion.glitch.me'
+# API_URL = 'https://imminent-jet-suggestion.glitch.me'
+API_URL = "http://127.0.0.1:5000"
 routes = web.RouteTableDef()
+start_button = [
+    [
+        KeyboardButton(text="/start")
+    ],
+    [
+        KeyboardButton(text="/help")
+    ],
+    [
+        KeyboardButton(text="/edit_profile")
+    ],
+    [
+        KeyboardButton(text="/stop")
+    ]
+]
+edit_user_buttons = [
+    [
+        KeyboardButton(text="/age")
+
+    ],
+    [
+        KeyboardButton(text="/name")
+    ],
+    [
+        KeyboardButton(text="/picture")
+    ],
+    [
+        KeyboardButton(text="/description")
+    ],
+    [
+        KeyboardButton(text="/end_edit_profile")
+    ]
+]
+kb = ReplyKeyboardMarkup(keyboard=start_button, resize_keyboard=True, one_time_keyboard=False)
+user_kb = ReplyKeyboardMarkup(keyboard=edit_user_buttons, resize_keyboard=True, one_time_keyboard=False)
 
 
 @dp.message(Command('start'))
@@ -48,18 +84,135 @@ async def start(message: types.Message, state):
         await prepare_link(message)
 
 
-async def prepare_link(message):
+@dp.message(Command("help"))
+async def bot_help(message: types.Message):
+    data = []
+    await message.answer("Это помощь")
+
+
+@dp.message(Command("edit_profile"))
+async def edit_user_info(message: types.Message):
+    await message.answer("Вы перешли в меню редактирования своего профиля", reply_markup=ReplyKeyboardRemove())
+    await message.answer("Теперь вы можете выбрать команду для редактирования", reply_markup=user_kb)
+
+
+@dp.message(Command("age"))
+async def edit_age(message: types.Message, state):
+    await message.answer("Введите новые данные для обновления возраста")
+    await state.set_state(WaitNewAge.waiting_message)
+
+
+class WaitNewAge(StatesGroup):
+    waiting_message = State()
+
+
+@dp.message(WaitNewAge.waiting_message)
+async def update_age(message: types.Message, state):
+    try:
+        age = int(message.text.rstrip())
+        if not (6 <= age <= 100):
+            raise ValueError
+        res = requests.put(f"{API_URL}/edit_user/{message.from_user.id}", json={"old": age}).json()
+        await state.clear()
+        await message.answer("Вы успешно изменили возраст!")
+
+    except ValueError:
+        await message.answer("Возраст выходит за рамки или не является чилом. Введите еще раз.")
+
+
+@dp.message(Command("name"))
+async def edit_name(message: types.Message, state):
+    await message.answer("Введите обновленное имя")
+    await state.set_state(WaitNewName.waiting_message)
+
+
+class WaitNewName(StatesGroup):
+    waiting_message = State()
+
+
+@dp.message(WaitNewName.waiting_message)
+async def update_name(message: types.Message, state):
+    name = message.text.rstrip()
+    res = requests.put(f"{API_URL}/edit_user/{message.from_user.id}", json={"name": name}).json()
+    await state.clear()
+    await message.answer("Вы успешно сменили имя!")
+
+
+@dp.message(Command("end_edit_profile"))
+async def end_edit_profile(message: types.Message, state):
+    await state.clear()
+    await message.answer("Все данные успешно обновлены!", reply_markup=kb)
+
+
+@dp.message(Command("picture"))
+async def edit_picture(message: types.Message, state):
+    await message.answer("Загрузите фото, которое вы хотите установить")
+    await state.set_state(WaitNewPicture.wait_picture)
+
+
+class WaitNewPicture(StatesGroup):
+    wait_picture = State()
+
+
+@dp.message(WaitNewPicture.wait_picture)
+async def update_picture(message: types.Message, state):
+    try:
+        file_id = message.photo[-1].file_id
+        photo = await bot.get_file(file_id)
+        file_path = photo.file_path
+
+        file_name = f"static/img/{message.from_user.id}.jpg"
+        file = await bot.download_file(file_path)
+        res = requests.post(f"{API_URL}/create_picture",
+                            files={"file": (file_name, file, "image/jpg")}
+                            ).text
+        await state.clear()
+        await message.answer("Вы успешно обновили свою фотографию!")
+    except TypeError:
+        await message.answer("Вы не загрузили фотографию! Повторите попытку.")
+
+
+@dp.message(Command("description"))
+async def edit_description(message: types.Message, state):
+    await message.answer("Опишите себя, это увидят другие пользователи.")
+    await state.set_state(WaitNewDescription.wait_description)
+
+
+class WaitNewDescription(StatesGroup):
+    wait_description = State()
+
+
+@dp.message(WaitNewDescription.wait_description)
+async def update_description(message: types.Message, state):
+    text = message.text
+    res = requests.put(f"{API_URL}/edit_user/{message.from_user.id}", json={"capture": text}).json()
+    await state.clear()
+    await message.answer("Данные успешно сохранены")
+
+
+async def prepare_link(message, new_user=False):
     payload = {"user_id": message.from_user.id, 'm': message.text}
     json_str = json.dumps(payload)
     encoded = urllib.parse.quote(json_str)
     url = f"{API_URL}/?data={encoded}"
-    inline_kb = InlineKeyboardMarkup()
-    inline_kb.add(InlineKeyboardButton(text=u'🚀 Открыть Web App', url=url))
-
-    await message.answer(
-        u"Привет! 👋\nНажми кнопку ниже, чтобы открыть наше веб-приложение:",
-        reply_markup=inline_kb
-    )
+    btns = [
+        [
+            InlineKeyboardButton(text=u'🚀 Открыть Web App', url=url)
+        ]
+    ]
+    inline_kb = InlineKeyboardMarkup(inline_keyboard=btns)
+    # inline_kb.add(InlineKeyboardButton(text=u'🚀 Открыть Web App', url=url))
+    if new_user:
+        await message.answer(
+            u"Нажми кнопку ниже, чтобы открыть наше веб-приложение:",
+            reply_markup=inline_kb
+        )
+    else:
+        await message.answer(u"Привет! 👋", reply_markup=kb)
+        await message.answer(
+            u"Нажми кнопку ниже, чтобы открыть наше веб-приложение:",
+            reply_markup=inline_kb
+        )
     # inline_kb = InlineKeyboardMarkup(inline_keyboard=[
     #     [InlineKeyboardButton(text='🚀 Открыть Web App', web_app=WebAppInfo(url=url))]
     # ])
@@ -81,7 +234,7 @@ class BirthdateForm(StatesGroup):
 
 
 async def ask_birthdate(message: types.Message, state):
-    await message.answer("Привет! Скажи сколько тебе лет.")
+    await message.answer("Привет! Скажи сколько тебе лет.", reply_markup=kb)
     await state.set_state(BirthdateForm.waiting_for_birthdate)
 
 
@@ -95,7 +248,7 @@ async def process_birthdate(message: types.Message, state):
         picture_path = await get_user_avatar(message)
         requests.post(f"{API_URL}/create_user", json=make_reg(message, age, picture_path))
         await state.clear()
-        await prepare_link(message)
+        await prepare_link(message, new_user=True)
     except ValueError:
         await message.answer("Возраст выходит за рамки или не является чилом. Введите еще раз.")
 
